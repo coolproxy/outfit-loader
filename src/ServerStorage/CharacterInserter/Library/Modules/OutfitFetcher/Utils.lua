52d2b745d4a2ceaf61519c34d2c27773c3df2894
coolproxy <76964407+coolproxy@users.noreply.github.com>
2021-01-04 10:33:22 -0800

    Update Utils.lua

--- src/ServerStorage/CharacterInserter/Library/Modules/OutfitFetcher/Utils.lua
+++ src/ServerStorage/CharacterInserter/Library/Modules/OutfitFetcher/Utils.lua
@@ -1,48 +1,47 @@
 local http = game:GetService("HttpService")
-local utils = {};  -- OOP module
+local utils = {}
 local cache = {}
 
 function utils.get(url)
 	local result;
 	local success, err = pcall(function()
-		result = http:GetAsync(url);
+		result = http:GetAsync(url)
 	end)
 	
 	if success and not err then
-		return result;
+		return result
 	else
 		warn('Error occured, "'..err.."'")
-		return err;
+		return err
 	end
 end
 
 function utils.encode(data) -- json encode
-	return http:JSONEncode(data);
+	return http:JSONEncode(data)
 end
 
 function utils.decode(data) -- json decode
-	return http:JSONDecode(data);
+	return http:JSONDecode(data)
 end
 
-function utils.getinfo(tbl) -- getting info from avatar table
-	local data = {}; -- new info
-	for index,value in pairs(tbl.data) do -- loop through
-		local name = value.name; -- the name of an outfit
-		local id = value.id; -- outfit id (incredibly important);
-		data[name] = id; -- setting
+function utils.getinfo(tbl)
+	local data = {}
+	for index,value in pairs(tbl.data) do
+		local name = value.name
+		local id = value.id
+		data[name] = id
 	end
-	return data; -- return
+	return data
 end
 
-function utils.getTemplateId(assetId) -- getting tshirt stuff
-	local key = '.'..assetId; -- just incase SOMEONE has the outfit id of a shirt
+function utils.getTemplateId(assetId)
+	local key = '.' .. assetId
 	if cache[key] then
-		return cache[key];
+		return cache[key]
 	else
-		--local info = utils.get("https://jumpingstudios.xyz/robloxapi/getIdfromAsset.php?assetId=" .. assetId);
-		local info = utils.get("https://coolestwebsiteverrr.000webhostapp.com/getTemplate.php?assetId=" .. assetId);
-		cache[key] = info;
-		return info;
+		local info = utils.get("https://coolestwebsiteverrr.000webhostapp.com/getTemplate.php?assetId=" .. assetId)
+		cache[key] = info
+		return info
 	end
 end
 
