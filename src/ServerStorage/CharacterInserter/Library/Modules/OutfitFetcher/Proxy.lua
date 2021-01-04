1508bae8308d80b19995c655cf465200853793f2
coolproxy <76964407+coolproxy@users.noreply.github.com>
2021-01-04 10:33:54 -0800

    Update Proxy.lua

--- src/ServerStorage/CharacterInserter/Library/Modules/OutfitFetcher/Proxy.lua
+++ src/ServerStorage/CharacterInserter/Library/Modules/OutfitFetcher/Proxy.lua
@@ -1,12 +1,9 @@
--- todo: replace proxy
-
 local config = {
 	proxy = "https://coolestwebsiteverrr.000webhostapp.com/proxy.php"
-	--proxy = "https://jumpingstudios.xyz/robloxapi/proxy.php"; -- My own host, you can check out the setup tutorial if you want to use your own host.
 }
 
-local utils = require(script.Parent:WaitForChild("Utils")); -- Utilities module
+local utils = require(script.Parent:WaitForChild("Utils"))
 
 return function(url)
-	return utils.get(config.proxy .. '?url=' .. url) -- get request for the url specified (avatar.roblox.com) only.
+	return utils.get(config.proxy .. '?url=' .. url)
 end
